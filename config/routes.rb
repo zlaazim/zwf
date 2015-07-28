a900619df5afad19988402f15a1b2d0c892ecfe3
Zak <laazzak@gmail.com>
2015-07-28 11:07:54 +1000

    add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
-  get    'contact' => 'static_pages#contact'
+  get    'contacts' => 'contacts#new'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :contacts, only: [:new, :create]
 end
